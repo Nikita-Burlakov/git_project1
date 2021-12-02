4f14586c7f3a60e2ef2184b90633e71acfdf7179
Никита <nikita225@list.ru>
2021-12-02 20:02:01 +0300

    Крест

--- git_test_1.py
+++ git_test_1.py
@@ -1,9 +1,32 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('git git git')
-    print('UFO came and added this line')
+import pygame
+#import random
 
 
-if __name__ == '__main__':
-    main()
+def draw(screen, wid, height):
+    screen.fill((0, 0, 0))
+    pygame.draw.line(screen, (255, 255, 255), (0, 0),
+                     (wid - 1, height - 1), width=5)
+    pygame.draw.line(screen, (255, 255, 255), (wid - 1, 0),
+                     (0, height - 1), width=5)
+
+
+def main(wid, height):
+    pygame.init()
+    pygame.display.set_caption('Крест')
+
+    size = wid, height
+    screen = pygame.display.set_mode(size)
+
+    draw(screen, wid, height)
+    pygame.display.flip()
+
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
+
+
+try:
+    w, h = map(int, input().split())
+    main(w, h)
+except:
+    print('Неправильный формат ввода')
